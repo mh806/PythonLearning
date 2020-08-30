b8d36ad730c2659ae98de8545aaa01632c956087
willhuang0204 <william_mj_huang@hotmail.com>
2020-08-30 21:24:38 +0800

    30 Aug Learning Python

---
+++ Round Function.py
@@ -0,0 +1,7 @@
+import math
+
+print(round(1.3333, 1))
+print(round(5.215 / 2, 1))
+
+print(math.ceil(3.666))
+print(math.floor(1.211), end="\n")
---
+++ Variable.py
@@ -0,0 +1,23 @@
+import math
+
+age1 = 5 # good
+# variable name - identifier
+# variable usage - prevent reductant codes,store value,more readable
+
+"""
+print(val)
+"""
+result = 10//3
+
+# give variable name meaningful
+print(age1)
+print(result)
+
+# / --> float
+# // --> integer
+
+print(3 * 3 * 3)
+print(3 ** 5)
+
+power = math.pow(3, 5)
+print(math.floor(power))
