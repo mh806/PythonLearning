a938ed60f8403f19facfbe08430038b9c87e5f8a
willhuang0204 <william_mj_huang@hotmail.com>
2020-08-31 23:01:08 +0800

    31 Aug Learning Python

---
+++ String.py
@@ -0,0 +1,26 @@
+msg = "Hello World"
+print(msg)
+
+msg1 = "\x41\'re good"
+print(msg1)
+
+# Concatenation -> combine String
+print(msg, msg1)
+print(msg + "..." + msg1)
+
+# Expecting 1 argument --> concatenation
+longstring = ("This is a very long string..."
+              "hello world")
+
+print(longstring)
+
+poem = """This is all combined\
+        as one happy string...
+        what was that sound?\
+        it was a doorbell, ring\
+        when i see you, my heart sings
+        here plz take this diamond ring"""
+# """ = ()
+print(poem)
+
+# 23
