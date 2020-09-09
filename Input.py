f6f99e1a5a8398027fac1e13f057f6961eccfe13
willhuang0204 <william_mj_huang@hotmail.com>
2020-09-09 23:47:18 +0800

    09 Sep Learning Python

---
+++ Input.py
@@ -0,0 +1,14 @@
+# interaction: input and output
+print("Hey whats your name?")
+name = input()
+print("Hello, "+name)
+
+print("Whats your favourite number")
+fav_num = int(input())
+fav_num2 = int(input())
+print(fav_num+fav_num2)
+
+# How to convert string to hunumbers - Type casting
+print(type(str(5.6)))
+print(str(fav_num) + "+" + str(fav_num2) + "=" + str(fav_num+fav_num2))
+# 41
