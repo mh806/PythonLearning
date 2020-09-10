d61ad6087eb658f3b41b251e3a507698e2fce09c
willhuang0204 <william_mj_huang@hotmail.com>
2020-09-10 22:53:42 +0800

    10 Sep Learning Python

--- Input.py
+++ Input.py
@@ -11,4 +11,3 @@ print(fav_num+fav_num2)
 # How to convert string to hunumbers - Type casting
 print(type(str(5.6)))
 print(str(fav_num) + "+" + str(fav_num2) + "=" + str(fav_num+fav_num2))
-# 41
---
+++ Logic.py
@@ -0,0 +1,27 @@
+happy = True
+nothappy = False
+
+if not happy:
+    print("Hello World")
+else:
+    print("So sad")
+
+print("Whats your age")
+age = int(input())
+print(type(happy))
+print(age < 23)
+
+can_drink = age > 20
+print(can_drink)
+
+if 20 < age < 65:
+    print("Welcome to our app!")
+elif not age == 65:
+    print("Wow!!!")
+elif 65 < age < 78:
+    print("Discount price")
+else:
+    print("Thanks for trying app")
+
+# Indentation Error -> Tab vs Space
+# 51
