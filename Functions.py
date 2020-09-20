bbaf6530108430bf251fa7bf7b45992c1b264ceb
willhuang0204 <william_mj_huang@hotmail.com>
2020-09-20 22:26:00 +0800

    20 Sep Python Learning

---
+++ Functions.py
@@ -0,0 +1,17 @@
+def great(name="User", /, *, be_nice=True):
+    if not be_nice:
+        return "Who do you think you are"
+    print("Hey, William")
+    print("Welcome " + name)
+
+
+great("William")
+great()
+great("Nice", be_nice=True)
+# benefit -> save lines of codes, dont repeat codes, update one spot
+
+# arguments and parameters
+# arguments -> what you passed to functions
+# parameters -> saved in functions
+
+# 95
